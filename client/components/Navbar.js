2e77f3ed4c5e279ec779a92f74a9bd8307e4acd1
Zoë Davidson <zoe.a.h.davidson@gmail.com>
2021-06-30 09:20:57 -0400

    Link working to stories and individual stories

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,12 +1,13 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = (props) => {
   return (
-    <div id='navbar' className='row'>
-      <a>Stories</a>
+    <div id="navbar" className="row">
+      <Link to="/stories">Stories</Link>
       <a>Authors</a>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- client/components/StoriesList.js
+++ client/components/StoriesList.js
@@ -1,31 +1,30 @@
-import React from 'react'
-import {connect} from 'react-redux'
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 const StoriesList = (props) => {
-  const stories = props.stories
+  const stories = props.stories;
   return (
-    <div id='stories' className='column'>
-      {
-        stories.map(story => (
-          <div className='story' key={story.id}>
-            <a>
-              <h3>{story.title}</h3>
-            </a>
-            <a>
-              <p>{story.author.name}</p>
-            </a>
-            <hr />
-          </div>
-        ))
-      }
+    <div id="stories" className="column">
+      {stories.map((story) => (
+        <div className="story" key={story.id}>
+          <Link to={`/stories/${story.id}`}>
+            <h3>{story.title}</h3>
+          </Link>
+          <a>
+            <p>{story.author.name}</p>
+          </a>
+          <hr />
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
 const mapStateToProps = (state) => {
   return {
-    stories: state.stories
-  }
-}
+    stories: state.stories,
+  };
+};
 
-export default connect(mapStateToProps)(StoriesList)
+export default connect(mapStateToProps)(StoriesList);
