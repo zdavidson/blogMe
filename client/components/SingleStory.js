2dc6a36ec9cc37adc46f717e5ebf50b89cf5de40
Zoë Davidson <zoe.a.h.davidson@gmail.com>
2021-06-30 09:14:39 -0400

    fetching single story successfully

--- client/components/SingleStory.js
+++ client/components/SingleStory.js
@@ -1,40 +1,34 @@
 import { Component } from "react";
 import { connect } from "react-redux";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Comments from "./Comments";
 
-class SingleStory extends Component {
-  constructor() {
-    super();
-  }
-  render() {
-    const fakeStory = {
-      title: "Ships",
-      id: 2,
-      content:
-        "A ship in port is safe,\nbut that's not what ships are built for",
-      author: {
-        id: 1,
-        name: "Grace Hopper",
-      },
-      comments: [
-        {
-          id: 4,
-          content: "I like ships!",
-          author: {
-            id: 2,
-            name: "Alan Turing",
-            imageUrl: "default-author.png",
-          },
-        },
-      ],
-    };
-    const storyComments = fakeStory.comments;
-    const split = fakeStory.content.split("\n");
+const SingleStory = (props) => {
+  const [story, setStory] = useState({});
+  const [isLoaded, setIsLoaded] = useState(false);
 
+  const storyId = props.match.params.storyId;
+
+  useEffect(() => {
+    async function fetchStory() {
+      await fetch(`/api/stories/${storyId}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setStory(result);
+          setIsLoaded(true);
+        });
+    }
+    fetchStory();
+  }, []);
+
+  if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    const storyComments = story.comments;
+    const split = story.content.split("\n");
     return (
       <div id="single-story" className="column">
-        <h1>{fakeStory.title}</h1>
+        <h1>{story.title}</h1>
         {split.map((line) => {
           return <p key={line.length - 1}>{line}</p>;
         })}
@@ -43,6 +37,6 @@ class SingleStory extends Component {
       </div>
     );
   }
-}
+};
 
 export default SingleStory;
